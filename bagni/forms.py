a59e2978fdf75762599330ec0c9c3c827bcd7c1a
flyingfrog <marco.bartolini@gmail.com>
2014-01-31 20:45:59 +0100

    added map to bagno editing page

--- bagni/forms.py
+++ bagni/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+from django.contrib.gis.forms import ModelForm, OSMWidget
 from bagni.models import Bagno
+
+class OmbrelloniOLWidget(OSMWidget):
+    #TODO: this thing does not work ... shit
+    class Media:
+        js = ('http://cdnjs.cloudflare.com/ajax/libs/openlayers/2.11/OpenLayers.min.js',)
 
 # Create the form class.
 class BagnoForm(ModelForm):
     class Meta:
         model = Bagno
         exclude = ['slug']
+        widgets = {
+                'point' : OSMWidget(),
+                }
+
