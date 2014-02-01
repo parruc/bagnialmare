979f3f55ff113ffc5c53ea625c231cc7a45a799c
Matteo Parrucci <parruc@gmail.com>
2014-02-01 21:03:56 +0100

    single selection possible for manager during registration

--- authauth/forms.py
+++ authauth/forms.py
@@ -1,11 +1,24 @@
-from django.forms import ModelForm
+from django.utils.datastructures import MultiValueDict, MergeDict
+from django import forms
+
 from .models import Manager
 
+class M2MSelect(forms.Select):
+    def value_from_datadict(self, data, files, name):
+        if isinstance(data, (MultiValueDict, MergeDict)):
+            return data.getlist(name)
+        return data.get(name, None)
+
+
 # Create the form class.
-class ManagerSignupForm(ModelForm):
+class ManagerSignupForm(forms.ModelForm):
     class Meta:
         model = Manager
         fields = ['bagni', 'privacy']
+        widgets = {
+            'bagni': M2MSelect()
+        }
+
     def save(self, user):
         m = Manager.objects.create(user=user)
         m.bagni = self.cleaned_data['bagni']
