d19c76709036b908ac622729019ca2cac4b660f8
Matteo Parrucci <parruc@gmail.com>
2014-02-02 11:59:52 +0100

    default editor for ombrellpni now is vim

--- dbutils/dumpdata.sh
+++ dbutils/dumpdata.sh
@@ -1,7 +1,4 @@
-
-
-
 cd /var/www/ombrelloni.it/django
-/var/www/ombrelloni.it/venv/bin/python manage.py dumpdata --exclude=auth --indent=2 > ombrelloni/fixtures/dump.json
+/var/www/ombrelloni.it/venv/bin/python manage.py dumpdata --indent=2 > ombrelloni/fixtures/dump.json
 git commit ombrelloni/fixtures/dump.json -m "db dump"
 git push
