2caaba710ecb17bd5db6362e26fe538bdf81625a
flyingfrog <marco.bartolini@gmail.com>
2014-02-09 18:38:09 +0100

    added factories for all models except Image

--- bagni/tests/factories.py
+++ bagni/tests/factories.py
@@ -1,8 +1,9 @@
+import string
+from random import random
+
 import factory
 from factory import fuzzy
-#import bagni
 from django.contrib import auth
-#import authauth
 from django.contrib.gis.geos import Point
 
 DEFAULT_PASSWORD = "password"
@@ -10,15 +11,15 @@ DEFAULT_PASSWORD = "password"
 class BagnoFactory(factory.DjangoModelFactory):
     FACTORY_FOR = 'bagni.Bagno'
     name = factory.Sequence(lambda n: "bagno%03d" % n)
-    mail = factory.Sequence(lambda n: "bagno%03d@example.com" % n)
-    number = factory.Sequence(lambda n: "%03d" % n)
-    point = factory.Sequence(lambda n: Point([40.0 + n, 50.0 + n]))
+    mail = factory.LazyAttribute(lamda o: o.name + "@example.com")
+    number = fuzzy.FuzzyText(lenght=3, chars=string.digits)
+    point = factory.LazyAttribute(lambda o: Point([50+random()*10, 30+random()*10]))
     description = fuzzy.FuzzyText(length=50)
 
 class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = auth.models.User
     username = factory.Sequence(lambda n: "user%03d" % n)
-    email = factory.Sequence(lambda n: "user%03d@example.com" % n)
+    email = factory.LazyAttribute(lamda o: o.username + "@example.com")
     password = factory.PostGenerationMethodCall('set_password', DEFAULT_PASSWORD)
 
 class ManagerFactory(factory.DjangoModelFactory):
@@ -34,3 +35,30 @@ class ManagerFactory(factory.DjangoModelFactory):
             for bagno in bagni:
                 self.bagni.add(bagno)
 
+class DistrictFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = 'bagni.District'
+    name = factory.Sequence(lambda n: "district%03d" % n)
+    description = fuzzy.FuzzyText(length=50)
+
+class MunicipalityFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = 'bagni.Municipality'
+    name = factory.Sequence(lambda n: "municipality%03d" % n)
+    description = fuzzy.FuzzyText(length=50)
+    district = factory.SubFactory(DistrictFactory)
+
+class LanguageFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = 'bagni.Language'
+    name = factory.Iterator(['en', 'fr', 'es', 'it', 'de'])
+    description = fuzzy.FuzzyText(length=50)
+
+class ServiceCategoryFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = 'bagni.ServiceCategory'
+    name = factory.Sequence(lambda n: "service_category%03d" % n)
+    description = fuzzy.FuzzyText(length=200)
+
+class ServiceFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = 'bagni.Service'
+    name = factory.Sequence(lambda n: "service%03d" % n)
+    description = fuzzy.FuzzyText(length=200)
+    category = factory.SubFactory(ServiceCategoryFactory)
+    free = fuzzy.FuzzyChoice([True, False])
