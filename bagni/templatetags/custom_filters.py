4f410d4daa395ed690d11879fd586e8b7958689b
Nicola Valentini <nicola.valentini@gmail.com>
2014-02-26 18:17:15 +0100

    More goodies added to the search result page

--- bagni/templatetags/custom_filters.py
+++ bagni/templatetags/custom_filters.py
@@ -5,3 +5,16 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+@register.filter
+def batch(thelist, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(thelist), n):
+        yield thelist[i:i+n]
