3cd2788f5a6dd460ef48af05e214e936e03e405d
root <root@dev.creepingserver.it>
2014-03-18 04:35:21 -0400

    db dump

--- ombrelloni/fixtures/dump.sql
+++ ombrelloni/fixtures/dump.sql
@@ -9,6 +9,23 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
+-- Name: ombrelloni; Type: DATABASE; Schema: -; Owner: ombrelloni
+--
+
+CREATE DATABASE ombrelloni WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C' LC_CTYPE = 'C';
+
+
+ALTER DATABASE ombrelloni OWNER TO ombrelloni;
+
+\connect ombrelloni
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
