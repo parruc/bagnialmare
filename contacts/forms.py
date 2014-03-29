4178eb854e28a066b1a5cd56359b3ba30258cae3
Matteo Parrucci <parruc@gmail.com>
2014-03-29 15:38:45 +0100

    Classes in contact form

--- contacts/forms.py
+++ contacts/forms.py
@@ -3,9 +3,12 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class ContactForm(forms.Form):
-    sender = forms.EmailField(label=_('Your email address'))
-    subject = forms.CharField(max_length=100, label=_('Subject'))
-    message = forms.CharField(label=_('Message'), widget=forms.Textarea)
+    sender = forms.EmailField(label=_('Your email address'),
+        widget=forms.TextInput(attrs={'class': 'form-control input-lg'}))
+    subject = forms.CharField(max_length=100, label=_('Subject'),
+        widget=forms.TextInput(attrs={'class': 'form-control input-lg'}))
+    message = forms.CharField(label=_('Message'),
+        widget=forms.Textarea(attrs={'class': 'form-control input-lg'}))
 
     cc_myself = forms.BooleanField(required=False,
         label=_('Send a copy to your address'), initial=True)
