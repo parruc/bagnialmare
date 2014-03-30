60f6b39e8b5ce1d604e3d03bf03b085a3183bcef
Nicola Valentini <nicola.valentini@gmail.com>
2014-03-30 23:48:02 +0200

    Styled contact form

--- contacts/forms.py
+++ contacts/forms.py
@@ -4,11 +4,11 @@ from django.utils.translation import ugettext_lazy as _
 
 class ContactForm(forms.Form):
     sender = forms.EmailField(label=_('Your email address'),
-        widget=forms.TextInput(attrs={'class': 'form-control input-lg'}))
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
     subject = forms.CharField(max_length=100, label=_('Subject'),
-        widget=forms.TextInput(attrs={'class': 'form-control input-lg'}))
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
     message = forms.CharField(label=_('Message'),
-        widget=forms.Textarea(attrs={'class': 'form-control input-lg'}))
+        widget=forms.Textarea(attrs={'class': 'form-control'}))
 
     cc_myself = forms.BooleanField(required=False,
         label=_('Send a copy to your address'), initial=True)
