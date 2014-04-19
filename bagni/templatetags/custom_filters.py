fb6f7da2f6ed6e6b9bce2b683f9b0630230b067b
flyingfrog <marco.bartolini@gmail.com>
2014-04-19 12:01:03 +0200

    nicer address in templates

--- bagni/templatetags/custom_filters.py
+++ bagni/templatetags/custom_filters.py
@@ -18,3 +18,15 @@ def batch(thelist, n):
     """
     for i in xrange(0, len(thelist), n):
         yield thelist[i:i+n]
+
+@register.filter
+def neighbourhood_municipality(bagno):
+    """ Return 'Neighborhood, Municipality' if they are different,
+    otherwise just 'Municipality'
+    """
+    neighbourhood_name = bagno.neighbourhood.name
+    municipality_name = bagno.neighbourhood.municipality.name
+    if neighbourhood_name.lower() != municipality_name.lower():
+        return neighbourhood_name + ', ' + municipality_name
+    return municipality_name
+
