898135779f72f3eb00455d45f28d8090435ed3d6
Marco Benvenuto <marco.benvenuto1@gmail.com>
2014-04-19 16:13:22 +0000

    Ordered homepage service categories.

--- bagni/views/bagni.py
+++ bagni/views/bagni.py
@@ -31,7 +31,7 @@ class BagniView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(BagniView, self).get_context_data(**kwargs)
         context.update({'districts': District.objects.all()})
-        context.update({'facility_categories': ServiceCategory.objects.all()})
+        context.update({'facility_categories': ServiceCategory.objects.all().order_by('order')})
         return context
 
 
