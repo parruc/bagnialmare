c98fa8cbfff71e878dfd0d8b3fa5facac468dd5c
Matteo Parrucci <parruc@gmail.com>
2014-04-30 13:42:54 +0200

    removed attribution in map

--- bagni/static/js/single_map.js
+++ bagni/static/js/single_map.js
@@ -7,7 +7,7 @@ $(function() {
     var bagno_address = $("#bagno_map").data("address")
     map = L.map('bagno_map').setView([bagno_y, bagno_x], 13);
     L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-        attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
+        attribution: '',
         subdomains: ['otile1','otile2','otile3','otile4'],
         maxZoom: 18
     }).addTo(map);
