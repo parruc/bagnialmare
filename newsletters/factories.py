273c8d90263f36e92600620b83f90135a1946ab0
Matteo Parrucci <parruc@gmail.com>
2014-05-23 02:00:22 +0200

    tests and factories

--- newsletters/factories.py
+++ newsletters/factories.py
@@ -1,52 +1,66 @@
 #import string
 from random import randrange
-from uuid import uuid4
 import factory
+from factory.fuzzy import FuzzyText
 
 from django.contrib.webdesign import lorem_ipsum
+from django.contrib.auth import models as auth_models
+
+from newsletters import models as newsletters_models
 
 
 DEFAULT_PASSWORD = "password"
 def random_email():
-    return lorem_ipsum.words(1) + "@" + lorem_ipsum.words(1) + ".com"
+    return "@".join(lorem_ipsum.words(2)) + ".com"
 
-class NewletterTemplateFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = 'newsletters.NewletterTemplate'
-    name = factory.fuzzy.FuzzyText(prefix="newsletter template ", length=15)
+class NewsletterTemplateFactory(factory.DjangoModelFactory):
+    #class Meta:
+    #    model = newsletters_models.NewsletterTemplate
+    FACTORY_FOR = newsletters_models.NewsletterTemplate
+    name = FuzzyText(prefix="newsletter template ", length=15)
     path = "newsletters/staff_template.html"
 
 
-class NewletterTargetFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = 'newsletters.NewletterTarget'
-    name = factory.fuzzy.FuzzyText(prefix="newsletter target ", length=15)
+class NewsletterTargetFactory(factory.DjangoModelFactory):
+    #class Meta:
+    #    model = newsletters_models.NewsletterTarget
+    FACTORY_FOR = newsletters_models.NewsletterTarget
+    name = FuzzyText(prefix="newsletter target ", length=15)
 
 
 class NewsletterFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = 'newsletters.Newsletter'
-    name = factory.fuzzy.FuzzyText(prefix="newsletter ", length=15)
+    #class Meta:
+    #    model = newsletters_models.Newsletter
+    FACTORY_FOR = newsletters_models.Newsletter
+    name = FuzzyText(prefix="newsletter ", length=15)
     subject = lorem_ipsum.words(randrange(1, 3))
     text = lorem_ipsum.paragraphs(randrange(1, 10))
     sent_to = None
     sent_on = None
-    target = factory.SubFactory(NewletterTargetFactory)
-    template = factory.SubFactory(NewletterTemplateFactory)
+    target = factory.SubFactory(NewsletterTargetFactory)
+    template = factory.SubFactory(NewsletterTemplateFactory)
 
 
-class NewletterSubscriptionFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = 'newsletters.NewletterSubscription'
+class NewsletterSubscriptionFactory(factory.DjangoModelFactory):
+    #class Meta:
+    #    model = newsletters_models.NewsletterSubscription
+    FACTORY_FOR = newsletters_models.NewsletterSubscription
     email = random_email()
-    target = factory.SubFactory(NewletterTargetFactory)
-    hash_field = uuid4().hex
+    target = factory.SubFactory(NewsletterTargetFactory)
 
 
-class UserFactory():
-    FACTORY_FOR = 'django.contrib.auth.models.User'
-    first_name = lorem_ipsum.words(1)
-    last_name = lorem_ipsum.words(1)
-    admin = False
+class NewsletterUserFactory(factory.DjangoModelFactory):
+    #class Meta:
+    #    model = newsletters_models.NewsletterUser
+    FACTORY_FOR = newsletters_models.NewsletterUser
+    user = factory.SubFactory(auth_models.User)
+    old_email = random_email()
+
+
+class UserFactory(factory.DjangoModelFactory):
+    #class Meta:
+    #    model = auth_models.User
+    FACTORY_FOR = auth_models.User
+    username = FuzzyText(prefix="user_", length=5)
     email = random_email()
     password = DEFAULT_PASSWORD
-
-class NewsletterUserFactory():
-    user = factory.SubFactory(NewletterTargetFactory)
-    old_email = random_email()
--- newsletters/tests.py
+++ newsletters/tests.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 
-from factories import NewletterTemplateFactory
-from factories import NewletterTargetFactory
-from factories import NewletterSubscriptionFactory
-from factories import NewletterFactory
-from factories import NewsletterUserFactory
+from factories import NewsletterTemplateFactory
+from factories import NewsletterTargetFactory
+from factories import NewsletterSubscriptionFactory
+from factories import NewsletterFactory
 from factories import UserFactory
 import models
 
@@ -16,16 +15,19 @@ class NewsletterTest(TestCase):
         """
         This already tests wether we can create a new object or not
         """
-        self.newsletter_template = NewletterTemplateFactory()
-        self.newsletter_targets = [NewletterTargetFactory(), NewletterTargetFactory()]
-        self.newsletters = [NewletterFactory(target=self.newsletter_target_1),
-                            NewletterFactory(target=self.newsletter_target_2), ]
-        self.subscriptions = [NewletterSubscriptionFactory(target=self.newsletter_target_1, email="target_1@example.com"),
-                              NewletterSubscriptionFactory(target=self.newsletter_target_2, email="target_2@example.com"),
-                              NewletterSubscriptionFactory(target=self.newsletter_target_1, email="change_or_remove_me@example.com"),
-                              NewletterSubscriptionFactory(target=self.newsletter_target_2, email="change_or_remove_me@example.com"), ]
+        self.newsletter_template = NewsletterTemplateFactory()
+        self.newsletter_targets = [NewsletterTargetFactory(name="Target 1"), NewsletterTargetFactory(name="Target 2")]
+        self.newsletters = [NewsletterFactory(target=self.newsletter_targets[0]),
+                            NewsletterFactory(target=self.newsletter_targets[1]), ]
+        self.subscriptions = [NewsletterSubscriptionFactory(email="target_1@example.com", target=self.newsletter_targets[0], ),
+                              NewsletterSubscriptionFactory(email="target_2@example.com", target=self.newsletter_targets[1], ),
+                              NewsletterSubscriptionFactory(email="change_or_remove_me@example.com", target=self.newsletter_targets[0], ),
+                              NewsletterSubscriptionFactory(email="change_or_remove_me@example.com", target=self.newsletter_targets[1], ), ]
         self.user = UserFactory(email="change_or_remove_me@example.com")
-        self.newsletter_user = NewsletterUserFactory(user=self.user)
+
+
+    def _count_subscriptions(self, email):
+        return models.NewsletterSubscription.objects.filter(email=email).count()
 
 
     def tearDown(self):
@@ -35,14 +37,24 @@ class NewsletterTest(TestCase):
         models.NewsletterTarget.objects.all().delete()
 
 
-    def test_subscription_changed(self):
+    def test_user_changes_email(self):
         """
         Test that, on user email change, also the subscriptions
         to that mail changes accordingly
         """
-        self.user.email="new_email@example.com"
+        self.assertEqual(self._count_subscriptions(email="change_or_remove_me@example.com"), 2)
+        self.assertEqual(self._count_subscriptions(email="new_email@example.com"), 0)
+        self.user.email = "new_email@example.com"
         self.user.save()
-        old_mail_count = NewletterSubscriptionFactory.objects.filter(email="change_or_remove_me@example.com").count()
-        new_mail_count = NewletterSubscriptionFactory.objects.filter(email="new_email@example.com").count()
-        self.assertEqual(old_mail_count, 0)
-        self.assertEqual(new_mail_count, 2)
+        self.assertEqual(self._count_subscriptions(email="change_or_remove_me@example.com"), 0)
+        self.assertEqual(self._count_subscriptions(email="new_email@example.com"), 2)
+
+    def test_subscription(self):
+        """
+        Test that usnubscribe view really unsubscribes user
+        """
+        self.assertEqual(self._count_subscriptions(email="change_or_remove_me@example.com"), 2)
+        self.assertEqual(self._count_subscriptions(email="new_email@example.com"), 0)
+        response = self.client.get('/newsletters/unsubscribe/target-1/change_or_remove_me@example.com', follow=True)
+
+
