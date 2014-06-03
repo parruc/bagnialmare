5d0061744c888a5f1cb9d71f6de5e5dbe0917b05
flyingfrog <marco.bartolini@gmail.com>
2014-06-03 19:32:05 +0200

    better messages

--- mailoffloader.py
+++ mailoffloader.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     if sys.argv and len(sys.argv) > 1:
         if os.path.exists(sys.argv[1]):
             sys.path.insert(0, sys.argv[1])
-            logger.info("insert %s into path" % (sys.argv[1],))
-    logger.info("resulting ENV:")
-    logger.info(os.environ)
-    logger.info("resulting SYS.PATH:")
-    logger.info(sys.path)
+            logger.debug("insert %s into path" % (sys.argv[1],))
+    logger.debug("resulting ENV:")
+    logger.debug(os.environ)
+    logger.debug("resulting SYS.PATH:")
+    logger.debug(sys.path)
     from django.conf import settings
     from django.utils.importlib import import_module
     logger.debug("found socket: %s" % settings.MAILOFFLOADER_SOCKET)
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     context = zmq.Context()
     server = context.socket(zmq.REP)
     server.bind(settings.MAILOFFLOADER_SOCKET)
+    logger.info("server listening on socket: %s" % settings.MAILOFFLOADER_SOCKET)
     while True:
         msg = server.recv_json()
         #send_mass_html_mail(msg['subject'], msg['text_content'], msg['html_content'], msg['recipients'], msg['from_email'], msg['test']
