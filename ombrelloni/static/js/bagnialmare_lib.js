f5a7a8cc3b3789ef87bfc5df319bdc18bf38953a
Matteo Parrucci <parruc@gmail.com>
2014-06-04 19:36:36 +0200

    back to default map markers

--- ombrelloni/static/js/bagnialmare_lib.js
+++ ombrelloni/static/js/bagnialmare_lib.js
@@ -15,9 +15,12 @@ function hideSpinner(selector)
 
 
 var ombrelloneIcon = L.icon({
-    iconUrl: '/static/js/images/ombrellone.png',
-
-    iconSize:     [32, 32], // size of the icon
-    iconAnchor:   [22, 30], // point othe icon which will correspond to marker's location
-    popupAnchor:  [-6, -27] // point from which the popup should open relative to the iconAnchor
+    //iconUrl: '/static/js/images/ombrellone.png',
+    iconUrl: '/static/js/images/marker-icon-2x.png',
+    iconSize: [25,41],
+    iconAnchor: [12, 41],
+    popupAnchor: [-3, -76],
+    shadowUrl: '/static/js/images/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [9, 40]
 });
