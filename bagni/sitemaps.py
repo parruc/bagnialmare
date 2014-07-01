c4ae75338b5210f4145357b29a03aabf7641f6fc
flyingfrog <marco.bartolini@gmail.com>
2014-07-01 20:14:14 +0200

    added SEO pages to sitemap

--- bagni/sitemaps.py
+++ bagni/sitemaps.py
@@ -1,7 +1,10 @@
 from models.bagni import Bagno
+from models.places import Neighbourhood
 from models.services import Service
 
 from ombrelloni.common.sitemaps import LocalesSitemap, StaticLocalesSitemap
+
+from django.core.urlresolvers import reverse
 
 class BagnoSitemap(LocalesSitemap):
     def items(self):
@@ -15,8 +18,23 @@ class BagnoStatic(StaticLocalesSitemap):
     def items(self):
         return ['homepage', 'bagni', 'search']
 
+class NeighbourhoodFacilitySitemap(LocalesSitemap):
+    def items(self):
+        ret = []
+        for neighbourhood in Neighbourhood.objects.all():
+            for facility in Service.objects.all():
+                ret.append((neighbourhood, facility))
+        return ret
+
+    def location(self, obj):
+        return reverse("bagni-by-facility-and-neighbourhood",
+                kwargs={'facility_slug' : obj[1].slug,
+                        'neighbourhood_slug' : obj[0].slug,
+                        })
+
 SITEMAPS = {
     'bagni' : BagnoSitemap,
     'bagni-static': BagnoStatic,
+    'neighbourhood-services': NeighbourhoodFacilitySitemap,
 }
 
