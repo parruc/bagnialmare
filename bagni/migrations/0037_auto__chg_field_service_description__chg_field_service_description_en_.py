668e4373b56bd1a172a253ad57ffbdb160a6cc5b
4hm-build <4hm-build@matteoparrucci.it>
2014-07-22 05:35:23 -0400

    migration with description fields that become whyswig

---
+++ bagni/migrations/0037_auto__chg_field_service_description__chg_field_service_description_en_.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Service.description'
+        db.alter_column(u'bagni_service', 'description', self.gf('ckeditor.fields.RichTextField')(max_length=2000))
+
+        # Changing field 'Service.description_en'
+        db.alter_column(u'bagni_service', 'description_en', self.gf('ckeditor.fields.RichTextField')(max_length=2000, null=True))
+
+        # Changing field 'Service.description_it'
+        db.alter_column(u'bagni_service', 'description_it', self.gf('ckeditor.fields.RichTextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description_en'
+        db.alter_column(u'bagni_servicecategory', 'description_en', self.gf('ckeditor.fields.RichTextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description_it'
+        db.alter_column(u'bagni_servicecategory', 'description_it', self.gf('ckeditor.fields.RichTextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description'
+        db.alter_column(u'bagni_servicecategory', 'description', self.gf('ckeditor.fields.RichTextField')(max_length=2000))
+
+        # Changing field 'Bagno.description'
+        db.alter_column(u'bagni_bagno', 'description', self.gf('ckeditor.fields.RichTextField')(max_length=350))
+
+        # Changing field 'Bagno.description_en'
+        db.alter_column(u'bagni_bagno', 'description_en', self.gf('ckeditor.fields.RichTextField')(max_length=350, null=True))
+
+        # Changing field 'Bagno.description_it'
+        db.alter_column(u'bagni_bagno', 'description_it', self.gf('ckeditor.fields.RichTextField')(max_length=350, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Service.description'
+        db.alter_column(u'bagni_service', 'description', self.gf('django.db.models.fields.TextField')(max_length=2000))
+
+        # Changing field 'Service.description_en'
+        db.alter_column(u'bagni_service', 'description_en', self.gf('django.db.models.fields.TextField')(max_length=2000, null=True))
+
+        # Changing field 'Service.description_it'
+        db.alter_column(u'bagni_service', 'description_it', self.gf('django.db.models.fields.TextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description_en'
+        db.alter_column(u'bagni_servicecategory', 'description_en', self.gf('django.db.models.fields.TextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description_it'
+        db.alter_column(u'bagni_servicecategory', 'description_it', self.gf('django.db.models.fields.TextField')(max_length=2000, null=True))
+
+        # Changing field 'ServiceCategory.description'
+        db.alter_column(u'bagni_servicecategory', 'description', self.gf('django.db.models.fields.TextField')(max_length=2000))
+
+        # Changing field 'Bagno.description'
+        db.alter_column(u'bagni_bagno', 'description', self.gf('django.db.models.fields.TextField')(max_length=350))
+
+        # Changing field 'Bagno.description_en'
+        db.alter_column(u'bagni_bagno', 'description_en', self.gf('django.db.models.fields.TextField')(max_length=350, null=True))
+
+        # Changing field 'Bagno.description_it'
+        db.alter_column(u'bagni_bagno', 'description_it', self.gf('django.db.models.fields.TextField')(max_length=350, null=True))
+
+    models = {
+        'bagni.bagno': {
+            'Meta': {'object_name': 'Bagno'},
+            'accepts_booking': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('ckeditor.fields.RichTextField', [], {'max_length': '350', 'blank': 'True'}),
+            'description_en': ('ckeditor.fields.RichTextField', [], {'max_length': '350', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('ckeditor.fields.RichTextField', [], {'max_length': '350', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'languages': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'bagni'", 'blank': 'True', 'to': "orm['bagni.Language']"}),
+            'mail': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'neighbourhood': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'bagni'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': "orm['bagni.Neighbourhood']"}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'point': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True', 'blank': 'True'}),
+            'services': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'bagni'", 'blank': 'True', 'to': "orm['bagni.Service']"}),
+            'site': ('django.db.models.fields.URLField', [], {'max_length': '75', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        },
+        'bagni.district': {
+            'Meta': {'object_name': 'District'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.image': {
+            'Meta': {'object_name': 'Image'},
+            'bagno': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'images'", 'to': "orm['bagni.Bagno']"}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        },
+        'bagni.language': {
+            'Meta': {'object_name': 'Language'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2000'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.municipality': {
+            'Meta': {'object_name': 'Municipality'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'district': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'municipalities'", 'to': "orm['bagni.District']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.neighbourhood': {
+            'Meta': {'object_name': 'Neighbourhood'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'municipality': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'neighbourhoods'", 'to': "orm['bagni.Municipality']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.service': {
+            'Meta': {'object_name': 'Service'},
+            'aliases': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'aliases_en': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'aliases_it': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'services'", 'null': 'True', 'to': "orm['bagni.ServiceCategory']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'description_en': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'free': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'seo_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'seo_name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'seo_name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.servicecategory': {
+            'Meta': {'object_name': 'ServiceCategory'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'description_en': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'description_it': ('ckeditor.fields.RichTextField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_it': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug_it': ('autoslug.fields.AutoSlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'bagni.telephone': {
+            'Meta': {'object_name': 'Telephone'},
+            'bagno': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'telephones'", 'to': "orm['bagni.Bagno']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'name'", 'unique_with': '()'})
+        }
+    }
+
+    complete_apps = ['bagni']
