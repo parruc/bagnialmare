7c0a07f36832aa026a294f0e0c19bfc82d1f2907
Matteo PArrucci <parruc@gmail.com>
2019-06-01 00:36:23 +0000

    fixed maps

--- bagnialmare/static/js/OpenStreetMap.js
+++ bagnialmare/static/js/OpenStreetMap.js
@@ -14,9 +14,9 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
      */
     initialize: function(name, options) {
         var url = [
-            "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+            "//a.tile.openstreetmap.org/${z}/${x}/${y}.png",
+            "//b.tile.openstreetmap.org/${z}/${x}/${y}.png",
+            "//c.tile.openstreetmap.org/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 20,
@@ -47,9 +47,9 @@ OpenLayers.Layer.OSM.CycleMap = OpenLayers.Class(OpenLayers.Layer.OSM, {
      */
     initialize: function(name, options) {
         var url = [
-            "http://a.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
-            "http://b.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
-            "http://c.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"
+            "//a.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
+            "//b.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png",
+            "//c.tile.opencyclemap.org/cycle/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 19,
@@ -80,9 +80,9 @@ OpenLayers.Layer.OSM.TransportMap = OpenLayers.Class(OpenLayers.Layer.OSM, {
      */
     initialize: function(name, options) {
         var url = [
-            "http://a.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png",
-            "http://b.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png",
-            "http://c.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png"
+            "//a.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png",
+            "//b.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png",
+            "//c.tile2.opencyclemap.org/transport/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 19,
